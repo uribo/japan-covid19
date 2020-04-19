f1f44481592761eebdbacde476a56b8e195ac17a
Shinya Uryu <suika1127@gmail.com>
2020-04-19 11:02:20 +0900

    内閣官房公開の人流の減少率を追加
    株式会社Agoop提供、ＮＴＴドコモ「モバイル空間統計」分析レポートのデータ

---
+++ mobility.R
@@ -0,0 +1,15 @@
+library(jpcovid19)
+
+df_corona_gojp <-
+  c("agoop", "docomo") %>%
+  purrr::map_dfr(collect_corona_go_jp) %>% 
+  tibble::as_tibble()
+
+if (!file.exists("data-raw/corona_gojp_mobility.csv")) {
+  df_corona_gojp %>% 
+    readr::write_csv("data-raw/corona_gojp_mobility.csv")
+} else {
+  rbind(readr::read_csv("data-raw/corona_gojp_mobility.csv"),
+        df_corona_gojp) %>% 
+    readr::write_csv("data-raw/corona_gojp_mobility.csv")  
+}
