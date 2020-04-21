c7b2f8981ade6e99d2fb062821792b8e45640cb8
Shinya Uryu <suika1127@gmail.com>
2020-04-21 12:23:10 +0900

    Manual update

--- mobility.R
+++ mobility.R
@@ -3,7 +3,8 @@ library(jpcovid19)
 df_corona_gojp <-
   c("agoop", "docomo") %>%
   purrr::map_dfr(collect_corona_go_jp) %>% 
-  tibble::as_tibble()
+  tibble::as_tibble() %>% 
+  purrr::discard(names(.) == "category")
 
 if (!file.exists("data-raw/corona_gojp_mobility.csv")) {
   df_corona_gojp %>% 
@@ -11,5 +12,5 @@ if (!file.exists("data-raw/corona_gojp_mobility.csv")) {
 } else {
   rbind(readr::read_csv("data-raw/corona_gojp_mobility.csv"),
         df_corona_gojp) %>% 
-    readr::write_csv("data-raw/corona_gojp_mobility.csv")  
+    readr::write_csv("data-raw/corona_gojp_mobility.csv")
 }
