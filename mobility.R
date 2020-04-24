6b33800d29374dc07a5f6198225ebbcc3f939ccf
Shinya Uryu <suika1127@gmail.com>
2020-04-24 18:31:58 +0900

    ダッシュボードページ https://corona.go.jp/dashboard/ 掲載のデータに切り替え

--- mobility.R
+++ mobility.R
@@ -1,10 +1,16 @@
 library(jpcovid19)
 
+sources <- 
+  c("agoop", "docomo", "kddi")
+
 df_corona_gojp <-
-  c("agoop", "docomo") %>%
-  purrr::map_dfr(collect_corona_go_jp) %>% 
-  tibble::as_tibble() %>% 
-  purrr::discard(names(.) == "category")
+  sources %>% 
+  purrr::set_names(c("株式会社Agoop", 
+                     "NTTドコモ「モバイル空間統計」分析レポート", 
+                     "KDDI株式会社")) %>% 
+  purrr::map_dfr(~ collect_corona_go_jp(.x),
+                 .id = "source") %>% 
+  tibble::as_tibble()
 
 if (!file.exists("data-raw/corona_gojp_mobility.csv")) {
   df_corona_gojp %>% 
